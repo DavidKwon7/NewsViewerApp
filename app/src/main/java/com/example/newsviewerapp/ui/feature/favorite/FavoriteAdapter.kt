53389772f7a4339b3742c8ae139a9f1c1c099c1f
DavidKwon7 <khjunejune10@gmail.com>
2022-11-15 01:22:30 +0900

    FEAT: getFavoriteNews 구현

--- app/src/main/java/com/example/newsviewerapp/ui/feature/favorite/FavoriteAdapter.kt
+++ app/src/main/java/com/example/newsviewerapp/ui/feature/favorite/FavoriteAdapter.kt
@@ -1,4 +1,47 @@
 package com.example.newsviewerapp.ui.feature.favorite
 
-class FavoriteAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.domain.entity.Article
+import com.example.newsviewerapp.databinding.ItemFavoriteBinding
+
+class FavoriteAdapter() : ListAdapter<Article, FavoriteAdapter.FavoriteViewHolder>(DIFF_COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemFavoriteBinding.inflate(inflater, parent, false)
+        return FavoriteViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FavoriteViewHolder, position: Int) {
+        val item = getItem(position)
+        item?.let { holder.bind(it) }
+    }
+
+    inner class FavoriteViewHolder(
+        private val binding: ItemFavoriteBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(
+            item: Article
+        ) {
+            binding.article = item
+        }
+    }
+
+    companion object {
+
+        private val DIFF_COMPARATOR = object : DiffUtil.ItemCallback<Article>() {
+            override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+                return oldItem.url == newItem.url
+            }
+
+            override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
 }
--- app/src/main/java/com/example/newsviewerapp/ui/feature/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/newsviewerapp/ui/feature/favorite/FavoriteFragment.kt
@@ -5,19 +5,75 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.newsviewerapp.R
+import com.example.newsviewerapp.databinding.FragmentFavoriteBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class FavoriteFragment : Fragment() {
 
+    lateinit var binding: FragmentFavoriteBinding
     private val favoriteViewModel: FavoriteViewModel by viewModels()
+    private val favoriteAdapter: FavoriteAdapter by lazy {
+        FavoriteAdapter()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_favorite, container, false
+        )
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecyclerView()
+        getNews()
+        observeGetFavoriteNews()
+    }
+
+    private fun initRecyclerView() {
+        binding.rvList.apply {
+            adapter = favoriteAdapter
+        }
+    }
+
+    private fun getNews() {
+        favoriteViewModel.getNews()
+    }
+
+    private fun observeGetFavoriteNews() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                favoriteViewModel.favoriteStateFlow.collect { state ->
+                    when(state) {
+                        is FavoriteState.Empty -> {
+
+                        }
+                        is FavoriteState.Loading -> {
+
+                        }
+                        is FavoriteState.Success -> {
+                            val data = state.data
+                            favoriteAdapter.submitList(data)
+                        }
+                        is FavoriteState.Failed -> {
+
+                        }
+                    }
+                }
+            }
+        }
     }
 }
