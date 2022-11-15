b37a01e339ff6027e2ccc6df4547179c2ca7de41
DavidKwon7 <khjunejune10@gmail.com>
2022-11-15 14:19:09 +0900

    FEAT: timber 적용

--- app/src/main/java/com/example/newsviewerapp/MyApp.kt
+++ app/src/main/java/com/example/newsviewerapp/MyApp.kt
@@ -2,8 +2,14 @@ package com.example.newsviewerapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class MyApp : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
 }
